38909a1c9b9d7a07c6263da65fa3415170624004
Daniel Perrett <perrettdl@googlemail.com>
2019-01-31 17:39:42 +0000

    Use template literals for more readable source code

--- index.js
+++ index.js
@@ -7,8 +7,36 @@ const readline = require('readline');
 
 const hashes = stp.getHashes();
 
+const helpPrefix =`
+
+Generate unique, secure passwords for all of the websites you visit based on a single Master Password that you remember.
+
+Usage
+
+Start in interactive mode:
+
+    $ saltpass
+
+You will be prompted for your Master Password, Domain Name, Domain Phrase, and algorithm of choice.
+
+Start in interactive mode, but with a specific algorithm (you won't be prompted for this):
+
+    $ saltpass -a sha2
+
+Advanced users: pipe in your master password followed by domain names and phrases (tab separated, one per line) you want to use:
+
+    $ cat tempfile
+    some unguessable phrase
+    example.com     myname
+    example.net
+    example.org     myfullname
+    ^D
+    $ saltpass < tempfile
+
+`;
+
 const options = getopt.options([
-  [ 'algorithm|a=s',      { description: 'The hashing algorithm to use. One of ' + hashes.join(', '), test: hashes } ],
+  [ 'algorithm|a=s',      { description: `The hashing algorithm to use. One of ${hashes.join(', ')}.`, test: hashes } ],
   [ 'domain-name|n=s',    { description: "The Domain Name should match the website you're generating a password for." } ],
   [ 'domain-phrase|p=s',  { description: "The Domain Phrase is an optional field that can be used to differentiate multiple passwords on the same website." } ],
   [ 'keep|k!',            { description: "This flag determines whether to continue to prompt for more passwords once the first has been returned." } ],
@@ -16,35 +44,7 @@ const options = getopt.options([
 ], {
   name           : 'saltpass',
   commandVersion : version,
-  helpPrefix     : [
-    "",
-    "",
-    "Generate unique, secure passwords for all of the websites you visit based on a single Master Password that you remember.",
-    "",
-    "Usage",
-    "",
-    "Start in interactive mode:",
-    "",
-    "    $ saltpass",
-    "",
-    "You will be prompted for your Master Password, Domain Name, Domain Phrase, and algorithm of choice.",
-    "",
-    "Start in interactive mode, but with a specific algorithm (you won't be prompted for this):",
-    "",
-    "    $ saltpass -a sha2",
-    "",
-    "Advanced users: pipe in your master password followed by domain names and phrases (tab separated, one per line) you want to use:",
-    "",
-    "    $ cat tempfile",
-    "    some unguessable phrase",
-    "    example.com     myname",
-    "    example.net",
-    "    example.org     myfullname",
-    "    ^D",
-    "    $ saltpass < tempfile",
-    "",
-    "",
-  ].join("\n"),
+  helpPrefix     : helpPrefix,
   helpSuffix: [
     "This programme uses Nic Jansma's SaltThePass approach ( https://saltthepass.com/ ) and library. It depends on you remembering a single Master Password that you keep safe and only use on SaltThePass. Ideally, you should never disclose it to anyone else, or even write it down. The Master Password, Domain Name and (optionally) the Domain Phrase are combined and hashed to generate a different Salted Password for each website you visit.",
     "This programme, like SaltThePass is licensed under the MIT License."
